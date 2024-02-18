a191df576dce985a10d81d5e840edbe2140c92bc
takayama manabu <mt01work01@gmail.com>
2024-02-18 23:02:05 +0900

    <ログイン、会員登録P完成>

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -26,9 +26,8 @@
               <a class="header-nav__link" href="/mypage">マイページ</a>
             </li>
             <li class="header-nav__item">
-              <form>
-                <form class="form" action="/logout" method="post">
-                  @csrf
+              <form class="form" action="/logout" method="post">
+                @csrf
                 <button class="header-nav__button">ログアウト</button>
               </form>
             </li>
